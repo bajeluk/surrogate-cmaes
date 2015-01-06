d19ae09c1cdf9f848d92b6cdb5a8e85f3b5699e7
Lukas Bajer <bajeluk@seznam.cz>
2015-01-06 18:57:26 +0100

    Test module update -- calling Matlab in screen via SSH

--- exp/generateShellScripts.m
+++ exp/generateShellScripts.m
@@ -8,15 +8,32 @@ startIdxs = 1:combsPerMachine:(nCombinations-1);
 endIdxs =   combsPerMachine:combsPerMachine:nCombinations;
 
 % Generate .sh scripts
+fNameMng = [exppath filesep exp_id '_manager.sh'];
+fMng = fopen(fNameMng, 'w'); 
+fprintf(fMng, '#!/bin/sh\n');
+fprintf(fMng, '# Manager for experiment "%s", created on %s\n', exp_id, char(datetime('now')));
+
 for i = 1:nMachines
   machine = machines{i};
-  fid = fopen([exppath filesep exp_id '_' machine '.sh'], 'w'); 
+  fName = [exppath filesep exp_id '_' machine '.sh'];
+
+  fprintf(fMng, 'ssh %s@%s "screen -d -m \\"%s\\""\n', login, machine, fName);
+
+  fid = fopen(fName, 'w'); 
   fprintf(fid, '#!/bin/sh\n');
+  fprintf(fid, '# Script for experiment "%s" on machine "%s", created on %s\n', exp_id, machine, char(datetime('now')));
+  fprintf(fid, 'cd ~/prg/surrogate-cmaes; ulimit -t unlimited;\n');
   for id = startIdxs(i):endIdxs(i)
-    matlabCall = ['ssh ' login '@' machine ' "cd ~/prg/surrogate-cmaes; ulimit -t unlimited; nice -n 19 ' matlabcommand ' -r \\"bbob_test_01(' num2str(id) ', ''' exp_id ''', ''' exppath_short '''); exit\\""'];
+    fprintf(fid, 'echo ###########################################\n');
+    fprintf(fid, 'echo      Matlab call %d / %d\n', id - startIdxs(i)+1, combsPerMachine);
+    fprintf(fid, 'echo ###########################################\n');
+    matlabCall = ['nice -n 19 ' matlabcommand ' -nodisplay -r "bbob_test_01(' num2str(id) ', ''' exp_id ''', ''' exppath_short '''); exit"'];
     fprintf(fid, [matlabCall '\n']);
     disp(matlabCall);
   end
   fclose(fid);
+  fileattrib(fName, '+x');
 end
 
+fclose(fMng);
+fileattrib(fNameMng, '+x');
