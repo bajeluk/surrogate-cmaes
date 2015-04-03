4c30c5eed8f4f0041894098f9be192ccde3f564d
Lukas Bajer <bajeluk@seznam.cz>
2015-04-03 16:03:18 +0200

    timing experiment

--- exp/vendor/bbob/exampletiming.m
+++ exp/vendor/bbob/exampletiming.m
@@ -1,19 +1,31 @@
 % runs the timing experiment for MY_OPTIMIZER. fgeneric.m
 % and benchmarks.m must be in the path of MATLAB/Octave
 
-addpath('PUT_PATH_TO_BBOB/matlab');  % should point to fgeneric.m etc.
+exp_id = 'exp_geneEC_08_10D';
+dimensions = [2 5 10 20];
+id = 1; % ... for CMA-ES options
+maxfunevals = '100*dim';
+
+pathstr = fileparts(mfilename('fullpath'));
+exppath = [pathstr filesep '..' filesep '..' filesep 'experiments' filesep exp_id];
+
+load([exppath filesep 'scmaes_params.mat']);
+bbParams = getParamsFromIndex(id, bbParamDef, sgParamDef, cmParamDef);
+MY_OPTIMIZER = bbParams.opt_function;
+
+addpath(pathstr);  % should point to fgeneric.m etc.
 
 more off;  % in octave pagination is on by default
 
 timings = [];
 runs = [];
 dims = [];
-for dim = [2,3,5,10,20,40]
+for dim = dimensions
   nbrun = 0;
   ftarget = fgeneric('initialize', 8, 1, 'tmp');
   tic;
-  while toc < 30  % at least 30 seconds
-    MY_OPTIMIZER(@fgeneric, dim, ftarget, 1e5);  % adjust maxfunevals
+  while toc < 400  % at least 30 seconds
+    MY_OPTIMIZER(@fgeneric, dim, ftarget, myeval(maxfunevals), id);  % adjust maxfunevals
     nbrun = nbrun + 1;
   end  % while
   timings(end+1) = toc / fgeneric('evaluations');
