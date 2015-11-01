d2151ada507e61e10a5c3fc718c6a71193e56730
jdgregorian <jdgregorian@seznam.cz>
2015-11-01 14:20:37 +0100

    Repetition of unsuccessfull experiments improved

--- exp/util/runAgainErr.m
+++
@@ -1,9 +0,0 @@
-function runAgainErr(experiment, timebound)
-% Function runs again instances of 'experiment' which ended with error
-% setting up time boundary 'timebound'
-
-  errorList = dir(fullfile('exp', 'experiments', experiment, '*ERROR.mat'));
-  errorNumbers = sort(cell2mat(cellfun(@(x) str2double(x(strfind(x, 'D_') + 2: strfind(x, '_ERROR') -1)), {errorList(:).name}, 'UniformOutput', false)));
-  metacentrum_master_template('exp_restrEC_01', errorNumbers, timebound);
-  quit(0)
-end
