1be053973dc2f7e126ed20d31eb2a9557230f64c
Zbynek Pitra <jdgregorian@seznam.cz>
2016-04-05 14:07:10 +0200

    New experiments: Generation EC with y-normalization

---
+++ exp/experiments/exp_geneEC_10.m
@@ -0,0 +1,49 @@
+exp_id = 'exp_geneEC_10';
+exp_description = 'Surrogate CMA-ES model, GP and RF + y-normalization, generation EC, 24 functions, 15 instances';
+
+% BBOB/COCO framework settings
+
+bbobParams = { ...
+  'dimensions',         { 2, 3, 5, 10 }, ...
+  'functions',          num2cell(1:24), ...      % all functions: num2cell(1:24)
+  'opt_function',       { @opt_s_cmaes }, ...
+  'instances',          { [1:5, 41:50] }, ...    % default is [1:5, 41:50]
+  'maxfunevals',        { '250 * dim' }
+};
+
+% Surrogate manager parameters
+
+surrogateParams = { ...
+  'evoControl',         { 'generation' }, ...    % 'none', 'individual', 'generation', 'restricted'
+  'modelType',          { 'gp', 'rf' }, ...               % 'gp', 'rf', 'bbob'
+  'evoControlPreSampleSize', { 0 }, ...             % {0.25, 0.5, 0.75}, will be multip. by lambda
+  'evoControlIndividualExtension', { [] }, ...      % will be multip. by lambda
+  'evoControlBestFromExtension', { [] }, ...        % ratio of expanded popul.
+  'evoControlTrainRange', { 10 }, ...               % will be multip. by sigma
+  'evoControlTrainNArchivePoints', { '15*dim' },... % will be myeval()'ed, 'nRequired', 'nEvaluated', 'lambda', 'dim' can be used
+  'evoControlSampleRange', { 1 }, ...               % will be multip. by sigma
+  'evoControlOrigGenerations', { 1 }, ...
+  'evoControlModelGenerations', { 1, 5 }
+};
+
+% Model parameters
+
+modelParams = { ...
+  'useShift',           { false }, ...
+  'trainAlgorithm',     { 'fmincon' }, ...
+  'covFcn',             { '{@covMaterniso, 5}' }, ...
+  'hyp',                { struct('lik', log(0.01), 'cov', log([0.5; 2])) }, ...
+  'nBestPoints',        { 0 }, ...
+  'minLeaf',            { 2 }, ...
+  'inputFraction',      { 1 }, ...
+  'normalizeY',         { true }
+};
+
+% CMA-ES parameters
+
+cmaesParams = { ...
+  'PopSize',            { '(4 + floor(3*log(N)))' }, ...        %, '(8 + floor(6*log(N)))'};
+  'Restarts',           { 4 }, ...
+};
+
+logDir = '/storage/plzen1/home/bajeluk/public';
