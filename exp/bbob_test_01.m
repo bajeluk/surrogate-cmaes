850629a067dabade42fccb2c2309b7f894628006
repjak <j.repicky@gmail.com>
2016-04-13 22:28:59 +0200

    Add a script that groups results by a given parameter.

--- exp/bbob_test_01.m
+++ exp/bbob_test_01.m
@@ -249,21 +249,3 @@ function [exp_results, tmpFile, cmaes_out] = runTestsForAllInstances(opt_functio
   exp_results.y_evals = y_evals;
   exp_results.time = elapsedTime;
 end
-
-function printSettings(fid, exp_settings, exp_results, surrogateParams, cmaesParams)
-  fprintf(fid, '===== Experiment: %s =====\n\n', exp_settings.exp_id);
-  fprintf(fid, '== BBOB experiment settings: ==\n');
-  fprintf(fid, sprintfStruct(exp_settings));
-  fprintf(fid, '%15s: %f\n', 'time elapsed', exp_results.time);
-  fprintf(fid, '\n== Surrogate model parameters: ==\n');
-  fprintf(fid, sprintfStruct(surrogateParams));
-  fprintf(fid, '\n== CMA-ES parameters: ==\n');
-  fprintf(fid, sprintfStruct(cmaesParams));
-    fprintf(fid, '\n== CMA-ES surrogate model options: ==\n');
-  fprintf(fid, sprintfStruct(surrogateParams.modelOpts));
-  fprintf(fid, '\n== Numerical results: ==\n\n');
-  fprintf(fid, 'fbests:\n%s\n\n', num2str(exp_results.fbests));
-  fprintf(fid, 'f075:\n%s\n\n', num2str(exp_results.f075));
-  fprintf(fid, 'f050:\n%s\n\n', num2str(exp_results.f050));
-  fprintf(fid, 'f025:\n%s\n\n', num2str(exp_results.f025));
-end
--- exp/expInit.m
+++ exp/expInit.m
@@ -55,16 +55,6 @@ function expInit(exp_id)
 end
 
 
-function defs = generateParamDefStruct(carray)
-  assert(mod(length(carray), 2) == 0, 'The number of elements in field-value cell array is not even!')
-  defs = struct();
-  for i = 1:(length(carray)/2)
-    defs(i).name   = carray{(i-1)*2 + 1};
-    defs(i).values = carray{i*2};
-  end
-end
-
-
 function c = convertMultiStructToCell(st)
   c = cell(1,length(st));
   for i = 1:length(st)
