04abe2fda8b6e240de69be27c3d27309c2f4ed61
Zbynek Pitra <jdgregorian@seznam.cz>
2016-05-13 16:22:47 +0200

    Experiment settings printing in generateReport

--- exp/pproc/generateReport.m
+++ exp/pproc/generateReport.m
@@ -1,8 +1,10 @@
 function generateReport(expFolder)
-% generates report of experiment in folder
+% generateReport(expFolder) generates report of experiment in expFolder.
+%
+% See Also:
+%   relativeFValuesPlot
 
 %TODO: settings comparison - what is different between individual settings
-%TODO: rewrite printStructure to return string
  
   if nargin < 1
     help generateReport
@@ -16,8 +18,9 @@ function generateReport(expFolder)
   % load data
   params = load(paramFile);
   expName = params.exp_id;
-  BBfunc = cell2mat(getParam(params.bbParamDef, 'functions'));
-  dims   = cell2mat(getParam(params.bbParamDef, 'dimensions'));
+  settings = getSettings(params);
+  BBfunc = cell2mat(settings.bbParamDef.functions);
+  dims   = cell2mat(settings.bbParamDef.dimensions);
   nAlgs  = prod(cellfun(@length, [{params.cmParamDef.values}, {params.sgParamDef.values}]));
   
   
@@ -41,8 +44,16 @@ function generateReport(expFolder)
   fprintf(FID, '%% Created on %s in folder %s.\n', datestr(now), ppFolder);
   fprintf(FID, '\n');
   
+  % experiment settings
+  fprintf(FID, '%%%% Experiment settings\n');
+  fprintf(FID, '%% \n');
+  % keep only parameter fields
+  parSettings = rmfield(settings, {'exp_id', 'exppath_short', 'logDir'});
+  printStructure(parSettings, FID, 'StructName', '%%  ')
+  fprintf(FID, '\n');
+  
   % data loading
-  fprintf(FID, '%%%% load data\n');
+  fprintf(FID, '%%%% Load data\n');
   fprintf(FID, '\n');
   fprintf(FID, 'expFolder = ''%s'';\n', expFolder);
   fprintf(FID, 'resFolder = fullfile(expFolder, ''pproc'');\n');
@@ -51,30 +62,34 @@ function generateReport(expFolder)
   fprintf(FID, 'end\n');
   fprintf(FID, '\n');
   fprintf(FID, '%% loading results\n');
-  fprintf(FID, 'funcSet.BBfunc = ');
-  printStructure(BBfunc, FID)
-  fprintf(FID, ';\n');
-  fprintf(FID, 'funcSet.dims = ');
-  printStructure(dims, FID)
-  fprintf(FID, ';\n');
+  fprintf(FID, 'funcSet.BBfunc = %s;\n', printStructure(BBfunc, FID, 'Format', 'value'));
+  fprintf(FID, 'funcSet.dims = %s;\n', printStructure(dims, FID, 'Format', 'value'));
   fprintf(FID, '[exp_evals, exp_settings] = dataReady(expFolder, funcSet);\n');
   fprintf(FID, 'nSettings = length(exp_settings);\n');
   fprintf(FID, 'expData = arrayfun(@(x) exp_evals(:,:,x), 1:nSettings, ''UniformOutput'', false);\n');
   fprintf(FID, '\n');
   fprintf(FID, '%% create algorithm names\n');
-  algNames = arrayfun(@(x) ['ALG', num2str(x)], 1:nAlgs, 'UniformOutput', false);
-  fprintf(FID, 'expAlgNames = ');
-  printStructure(algNames, FID)
-  fprintf(FID, ';\n');
+  expAlgNames = arrayfun(@(x) ['ALG', num2str(x)], 1:nAlgs, 'UniformOutput', false);
+  printStructure(expAlgNames, FID)
   fprintf(FID, '\n');
   fprintf(FID, '%% color settings\n');
-  colors = randi(256, nAlgs, 3) - 1;
-  fprintf(FID, 'expCol = ');
-  printStructure(colors, FID)
-  fprintf(FID, ';\n');
+  expCol = randi(256, nAlgs, 3) - 1;
+  printStructure(expCol, FID);
   fprintf(FID, '\n');
   fprintf(FID, '%% load algorithms to compare\n');
-  fprintf(FID, 'alg = load(fullfile(''exp'', ''pproc'', ''compAlgMat.mat''));\n');
+  fprintf(FID, 'algMat = fullfile(''exp'', ''pproc'', ''compAlgMat.mat'');\n');
+  fprintf(FID, 'if ~exist(algMat, ''file'')\n');
+  fprintf(FID, '  try\n');
+  fprintf(FID, '    websave(algMat, ''http://artax.karlin.mff.cuni.cz/~bajel3am/scmaes/compAlgMat.mat'');\n');
+  fprintf(FID, '  catch\n');
+  fprintf(FID, '  end\n');
+  fprintf(FID, 'end\n');
+  fprintf(FID, 'try\n');
+  fprintf(FID, '  alg = load(algMat);\n');
+  fprintf(FID, '  compOn = true;\n');
+  fprintf(FID, 'catch\n');
+  fprintf(FID, '  compOn = false;\n');
+  fprintf(FID, 'end\n');
   fprintf(FID, 'algorithms = alg.algorithm;\n');
   fprintf(FID, '\n');
   
@@ -102,30 +117,53 @@ function generateReport(expFolder)
   % all algorithms comparison
   fprintf(FID, '%%%% All algorithms comparison\n');
   fprintf(FID, '\n');
-  fprintf(FID, 'data = [expData, {algorithms.data}];\n');
-  fprintf(FID, 'datanames = [expAlgNames, {algorithms.name}];\n');
-  fprintf(FID, 'colors = [expCol; cell2mat({algorithms.color}'')];\n');
-  fprintf(FID, '\n');
-  fprintf(FID, 'for f = funcSet.BBfunc\n');
-  fprintf(FID, '  %%%% \n');
-  fprintf(FID, '  close all\n');
+  fprintf(FID, 'if compOn\n');
+  fprintf(FID, '  data = [expData, {algorithms.data}];\n');
+  fprintf(FID, '  datanames = [expAlgNames, {algorithms.name}];\n');
+  fprintf(FID, '  colors = [expCol; cell2mat({algorithms.color}'')];\n');
   fprintf(FID, '  \n');
-  fprintf(FID, '  fprintf(''Function %%d\\n'', f)\n');
-  fprintf(FID, '  han = relativeFValuesPlot(data, ...\n');
-  fprintf(FID, '                            ''DataDims'', funcSet.dims, ...\n');
-  fprintf(FID, '                            ''DataFuns'', funcSet.BBfunc, ...\n');
-  fprintf(FID, '                            ''PlotFuns'', f, ...\n');
-  fprintf(FID, '                            ''AggregateDims'', false, ...\n');
-  fprintf(FID, '                            ''AggregateFuns'', false, ...\n');
-  fprintf(FID, '                            ''DataNames'', datanames, ...\n');
-  fprintf(FID, '                            ''Colors'', colors, ...\n');
-  fprintf(FID, '                            ''LegendOption'', ''out'', ...\n');
-  fprintf(FID, '                            ''Statistic'', @median);\n');
+  fprintf(FID, '  for f = funcSet.BBfunc\n');
+  fprintf(FID, '    %%%% \n');
+  fprintf(FID, '    close all\n');
+  fprintf(FID, '    \n');
+  fprintf(FID, '    fprintf(''Function %%d\\n'', f)\n');
+  fprintf(FID, '    han = relativeFValuesPlot(data, ...\n');
+  fprintf(FID, '                              ''DataDims'', funcSet.dims, ...\n');
+  fprintf(FID, '                              ''DataFuns'', funcSet.BBfunc, ...\n');
+  fprintf(FID, '                              ''PlotFuns'', f, ...\n');
+  fprintf(FID, '                              ''AggregateDims'', false, ...\n');
+  fprintf(FID, '                              ''AggregateFuns'', false, ...\n');
+  fprintf(FID, '                              ''DataNames'', datanames, ...\n');
+  fprintf(FID, '                              ''Colors'', colors, ...\n');
+  fprintf(FID, '                              ''LegendOption'', ''out'', ...\n');
+  fprintf(FID, '                              ''Statistic'', @median);\n');
+  fprintf(FID, '  end\n');
+  fprintf(FID, 'else\n');
+  fprintf(FID, '  warning(''Could not load nor download %%s. Omitting comparison of all algorithms.'', algMat);\n');
   fprintf(FID, 'end\n');
+  
+  % finalize
+  fprintf(FID, '\n');
+  fprintf(FID, 'close all\n');
   
   % close report file
   fclose(FID);
 
+end
+
+function settings = getSettings(params)
+% parses structure fields consisting 'name' and 'value' fields to structure
+  paramFields = fieldnames(params);
+  for i = 1:length(paramFields)
+    if isstruct(params.(paramFields{i})) && all(isfield(params.(paramFields{i}), {'name', 'values'}))
+      for f = 1:length(params.(paramFields{i}))
+        settings.(paramFields{i}).(params.(paramFields{i})(f).name) = ...
+          params.(paramFields{i})(f).values;
+      end
+    else
+      settings.(paramFields{i}) = params.(paramFields{i});
+    end
+  end
 end
 
 function value = getParam(setting, paramName)
--- exp/pproc/printStructure.m
+++ exp/pproc/printStructure.m
@@ -1,11 +1,25 @@
-function printStructure(structure, FID)
-% printStructure(structure, FID) prints all fields and values of 
+function str = printStructure(structure, FID, varargin)
+% printStructure(structure, FID, option) prints all fields and values of 
 % 'structure' to file 'FID'
 %
 % Input:
 %   structure - structure to print
 %   FID       - file identifier or filename | integer or string
+%   settings  - pairs of property (string) and value or struct with 
+%               properties as fields:
+%                 'StructName' - name of structure to be printed in result
+%                 'Format'     - format of resulting string:
+%                                  'value'  - returns only field values
+%                                  'field'  - does not return structure 
+%                                             name
 
+%TODO: include FID to varargin:
+%         printStructure(structure)
+%         printStructure(structure, FID)
+%         printStructure(structure, varargin)
+%         printStructure(structure, FID, varargin)
+
+  str = [];
   if nargin < 2
     FID = 1;
     if nargin < 1
@@ -14,24 +28,25 @@ function printStructure(structure, FID)
     end
   end
   
-  recentlyOpened = false;
-  if ischar(FID)
-    % printing results to txt file
-    resultname = FID;
-    FID = fopen(resultname, 'w');
-    assert(FID ~= -1, 'Cannot open %s !', resultname)
-    recentlyOpened = true;
+  if isstruct(varargin)
+    settings = varargin;
+  else
+    % keep cells as cells due to struct command
+    vararCellId = cellfun(@iscell, varargin);
+    varargin(vararCellId) = {varargin(vararCellId)};
+    settings = struct(varargin{:});
   end
   
-  % gain structure name
-  structureName = inputname(1);
+  % parse settings
+  structureName = defopts(settings, 'StructName', inputname(1));
   if isempty(structureName)
     structureName = 'structure';
   end
+  option = defopts(settings, 'Format', '');
   
   % create higher structure for structure array handling (due to function
   % subfields
-  extraStruct.(structureName) = structure;
+  extraStruct.s = structure;
   % gain all structure subfields
   settingsSF = subfields(extraStruct);
     
@@ -39,124 +54,161 @@ function printStructure(structure, FID)
   for sf = 1:length(settingsSF)
     % eval due to multiple subfields
     valueSF = eval(['extraStruct.', settingsSF{sf}]);
+    switch option
+      case {'value', 'values'}
+        strToPrint = '';
+      case {'field', 'fields'}
+        strToPrint = [settingsSF{sf}(3:end), ' = '];
+      otherwise
+        if strcmp(structureName(end), ' ') || ~isstruct(structure)
+          strToPrint = [structureName, settingsSF{sf}(3:end), ' = '];
+        else
+          strToPrint = [structureName, '.', settingsSF{sf}(3:end), ' = '];
+        end
+    end
+    str = prt(str, '%s', strToPrint);
     % array settings
     if numel(valueSF) > 1 && ~ischar(valueSF)
-      printArray(FID, valueSF);
+      str = printArray(str, valueSF);
     % non-array value
     else
-      printVal(FID, valueSF);
+      str = printVal(str, valueSF);
     end
-    if length(settingsSF) ~= 1
-      fprintf(FID,'\n');
+    if strcmp(option, 'value')
+      if length(settingsSF) ~= 1
+        str = prt(str, '\n');
+      end
+    else
+      str = prt(str, ';\n');
     end
+
   end
   
-  % close opened file
-  if recentlyOpened
-    fclose(FID);
+  if nargout < 1
+    recentlyOpened = false;
+    if ischar(FID)
+      % printing results to txt file
+      resultname = FID;
+      FID = fopen(resultname, 'w');
+      assert(FID ~= -1, 'Cannot open %s !', resultname)
+      recentlyOpened = true;
+    end
+
+    % print to file
+      fprintf(FID, str);
+      clear str
+
+    % close opened file
+    if recentlyOpened
+      fclose(FID);
+    end
   end
   
 end
 
-function printVal(FID, val)
+function str = prt(str, varargin)
+% adds string to str
+  str = [str, sprintf(varargin{:})];
+end
+
+function str = printVal(str, val)
 % function checks the class of value and prints it in appropriate format
-% to file FID
 
   if isempty(val)
     if iscell(val)
-      fprintf(FID,'{}');
+      str = prt(str, '{}');
     else
-      fprintf(FID,'[]');
+      str = prt(str, '[]');
     end
   elseif iscell(val) || (numel(val) > 1 && ~ischar(val))
   % cell or any kind of array (except char)
-    printArray(FID, val);
+    str = printArray(str, val);
   else
     switch class(val)
       case 'char'
-        fprintf(FID,'''%s''', val);
+        str = prt(str, '''%s''', val);
       case 'double'
         % NaN and Inf verification is part of condition because they cannot 
         % be converted to logicals
         if (isnan(val) || val == Inf || (mod(val,1) && abs(val) > 1))
-          fprintf(FID,'%f', val);
+          str = prt(str, '%f', val);
         elseif mod(val,1)
-          fprintf(FID,'%g', val);
+          str = prt(str, '%g', val);
         else
-          fprintf(FID,'%d', val);
+          str = prt(str, '%d', val);
         end
       case 'logical'
         if val
-          fprintf(FID,'true');
+          str = prt(str, 'true');
         else
-          fprintf(FID,'false');
+          str = prt(str, 'false');
         end
       case 'function_handle'
-        fprintf(FID,'@%s', func2str(val));
+        str = prt(str, '@%s', func2str(val));
       case 'struct'
-        printStruct(FID, val)
+        str = printStruct(str, val);
       otherwise
-        fprintf(FID,'%dx%d %s', size(val,1), size(val,2), class(val));
+        str = prt(str, '%dx%d %s', size(val,1), size(val,2), class(val));
     end
   end
 end
 
-function printArray(FID, val)
-% function prints array to file FID
+function str = printArray(str, val)
+% function prints array
 
   % cell array
   if iscell(val)
-    fprintf(FID,'{');
+    str = prt(str, '{');
     % first row
-    printVal(FID, val{1,1})
+    str = printVal(str, val{1,1});
     for c = 2:size(val,2)
-      fprintf(FID,', ');
-      printVal(FID, val{1,c})
+      str = prt(str, ', ');
+      str = printVal(str, val{1,c});
     end
     % rest of rows
     for r = 2:size(val,1)
-      fprintf(FID,'; ');
-      printVal(FID, val{r,1})
+      str = prt(str, '; ');
+      str = printVal(str, val{r,1});
       for c = 2:size(val,2)
-        fprintf(FID,', ');
-        printVal(FID, val{r,c})
+        str = prt(str, ', ');
+        str = printVal(str, val{r,c});
       end
     end
-    fprintf(FID,'}');
+    str = prt(str, '}');
   % other arrays
   else
-    fprintf(FID,'[');
+    str = prt(str, '[');
     % first row
-    printVal(FID, val(1,1))
+    str = printVal(str, val(1,1));
     for c = 2:size(val,2)
-      fprintf(FID,', ');
-      printVal(FID, val(1,c))
+      str = prt(str, ', ');
+      str = printVal(str, val(1,c));
     end
     % rest of rows
     for r = 2:size(val,1)
-      fprintf(FID,'; ');
-      printVal(FID, val(r,1))
+      str = prt(str, '; ');
+      str = printVal(str, val(r,1));
       for c = 2:size(val,2)
-        fprintf(FID,', ');
-        printVal(FID, val(r,c))
+        str = prt(str, ', ');
+        str = printVal(str, val(r,c));
       end
     end
-    fprintf(FID,']');
+    str = prt(str, ']');
   end
 end
 
-function printStruct(FID, s)
-% prints structure s to file FID
+function str = printStruct(str, s)
+% prints structure s
 
   sf = fieldnames(s);
-  fprintf(FID,'struct(');
-  fprintf(FID,'''%s'', ', sf{1});
-  printVal(FID, s.(sf{1}))
+  str = prt(str, 'struct(');
+  str = prt(str, '''%s'', ', sf{1});
+  str = printVal(str, s.(sf{1}));
   for fnum = 2 : length(sf)
-    fprintf(FID,', ''%s'', ', sf{fnum});
-    printVal(FID, s.(sf{fnum}))
+    str = prt(str, ', ''%s'', ', sf{fnum});
+    str = printVal(str, s.(sf{fnum}));
   end
-  fprintf(FID,')');
+  str = prt(str, ')');
 end
 
 function sf = subfields(ThisStruct)
