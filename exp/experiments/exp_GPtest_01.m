f06f66104dcdec8ab24535709aa474c150a9750c
Lukas Bajer <bajeluk@seznam.cz>
2017-03-28 13:18:16 +0200

    GP model testing -- modelStatistics.m updated

--- exp/experiments/exp_GPtest_01.m
+++ exp/experiments/exp_GPtest_01.m
@@ -1,83 +1,17 @@
-% exp_GPtest_01 model testing experiment -- Matlab part
-
-% model training experiment settings
-%
-% EXPID (filename w/o extension of this script) | string
-opts.exp_id     = 'exp_GPtest_01';
-% dataset name (filename w/o extension of the dataset) | string
-% or struct with the field '.ds' with 3D cell array with the data for {d, f, i}'s
-opts.dataset    = 'DTS_005';
-% type of model to test according to the ModelFactory | string
-opts.modelType = 'gp';
-% EXPPATH_SHORT
-opts.exppath_short = fullfile('exp', 'experiments');
-% statistics to compute
-opts.statistics = { 'mse', 'mzoe', 'kendall', 'rankmse', 'rankmzoe', 'rde' };
-
-% FUN/DIM/INST settings
-if (~exist('func', 'var') || isempty(func))
-  func = (1:24);  end
-if (~exist('dims', 'var') || isempty(dims))
-  dims = [2, 5, 10];  end
-if (~exist('instances', 'var') || isempty(instances))
-  instances = [1:5 41:50];  end
-
-% Maximal number of function evaluation per dimension to consider
-opts.maxEvals = 250;
-
-% path settings
-opts.scratch = getenv('SCRATCHDIR');
-
-% other settings
-opts.rewrite_results = false;
-
-% directory with the dataset and results
-opts.exppath = fullfile('exp', 'experiments', opts.exp_id);
-% specifying the dataset -- expand the filename if dataset is string
-if (ischar(opts.dataset))
-  opts.dataset = fullfile(opts.exppath, 'dataset', [opts.dataset, '.mat']);
-end
-% defSetFile = fullfile(scratch, 'model', 'defData', ['defSet_', num2str(maxEvals), 'FE.mat']);
-
-% default model options
-defModelOptions.useShift        = false;
-defModelOptions.predictionType  = 'sd2';
-defModelOptions.trainAlgorithm  = 'fmincon';
-defModelOptions.covFcn          = '{@covMaterniso, 5}';
-defModelOptions.normalizeY      = true;
-defModelOptions.hyp.lik         = log(0.01);
-defModelOptions.hyp.cov         = log([0.5; 2]);
-defModelOptions.covBounds       = [ [-2;-2], [25;25] ];
-defModelOptions.likBounds       = log([1e-6, 10]);
+modelOptions.useShift        = false;
+modelOptions.predictionType  = 'sd2';
+modelOptions.trainAlgorithm  = 'fmincon';
+modelOptions.covFcn          = '{@covMaterniso, 5}';
+modelOptions.normalizeY      = true;
+modelOptions.hyp.lik         = log(0.01);
+modelOptions.hyp.cov         = log([0.5; 2]);
+modelOptions.covBounds       = [ [-2;-2], [25;25] ];
+modelOptions.likBounds       = log([1e-6, 10]);
 
 % Full factorial design of the following parameters
-models1 = defModelOptions;      % deep copy (!)
-models1.trainsetType    = { 'nearestToPopulation', 'nearest', 'clustering', 'allPoints' };
-models1.trainRange      = { 1.0, 0.999 };
-models1.trainsetSizeMax = { '5*dim', '10*dim', '15*dim', '20*dim' };
-models1.meanFcn         = { 'meanConst', 'meanLinear' };
-models1.covFcn          = { '{@covSEiso}', '{@covSEard}', ...
+modelOptions.trainsetType    = { 'nearestToPopulation', 'nearest', 'clustering', 'allPoints' };
+modelOptions.trainRange      = { 1.0, 0.999 };
+modelOptions.trainsetSizeMax = { '5*dim', '10*dim', '15*dim', '20*dim' };
+modelOptions.meanFcn         = { 'meanConst', 'meanLinear' };
+modelOptions.covFcn          = { '{@covSEiso}', '{@covSEard}', ...
                             '{@covMaterniso, 5}', '{@covMaterniso, 3}' };
-
-defModel_options = combineFieldValues(defModelOptions);
-models1_options  = combineFieldValues(models1);
-
-% Combine default options and full factorial design
-%modelOptions     = [defModel_options; models1_options];
-modelOptions = models1_options;
-
-%% create testing dataset
-%ds = modelTestSets('exp_doubleEC_21_log15', func, dims, instances, opts);
-
-fprintf('== Summary of the testing assignment ==\n');
-fprintf('   # of models:  %d\n', length(modelOptions));
-fprintf('   functions:    %s\n', num2str(func));
-fprintf('   dimensions:   %s\n', num2str(dims));
-fprintf('   instances:    %s\n', num2str(instances));
-fprintf('=======================================\n');
-
-%% test chosen models
-modelFolders = testModels(modelOptions, opts, func, dims, instances);
-
-%% load and calculate results
-[rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, func, dims, instances);
--- exp/modelStatistics.m
+++ exp/modelStatistics.m
@@ -1,4 +1,4 @@
-function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functions, dimensions, instances, snapshots)
+function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functions, dimensions, instances, snapshots, exp_id)
 % modelStatistics --  creates a table with statistics,
 %                     each row represents model and number of snapshot (combined),
 %                     each column represents function and dimension (also combined),
@@ -17,14 +17,16 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
 %
 
   % Default input parameters settings
-  if (~exist('functions', 'var'))
+  if (~exist('functions', 'var') || isempty(functions))
     functions = 1; end
-  if (~exist('dimensions', 'var'))
+  if (~exist('dimensions', 'var') || isempty(dimensions))
     dimensions = 2; end
-  if (~exist('instances', 'var'))
+  if (~exist('instances', 'var') || isempty(instances))
     instances = [1:5, 41:50]; end
-  if (~exist('snapshots', 'var'))
+  if (~exist('snapshots', 'var') || isempty(snapshots))
     snapshots = [1 3 9]; end
+  if (~exist('exp_id', 'var') || isempty(exp_id))
+    exp_id = 'exp_GPtest_01'; warning('Using default exp_id = %s !', exp_id); end
 
   assert(isnumeric(functions), '''funcToTest'' has to be integer')
   assert(isnumeric(dimensions), '''dimsToTest'' has to be integer')
@@ -35,7 +37,7 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
   RDEs = cell(length(modelFolders), length(functions), length(dimensions));
   MSEs = cell(length(modelFolders), length(functions), length(dimensions));
   isTrained = cell(length(modelFolders), length(functions), length(dimensions));
-  modelOpts = [];
+  folderModelOptions = cell(1, length(modelFolders));
   % {
   % iterate through all specified models / dimensions / functions
   for i_model = 1:length(modelFolders)
@@ -57,18 +59,32 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
           warning('File %s not found', fileName);
           is_empty = true;
         else
+          
+          % load the model testing results
           data = load(fileName);
           data_instances = ismember(data.instances, instances);
-          if (isempty(modelOpts))
-            modelOpts = data.modelOptions;
+
+          % set up correct modelOptions
+          if (isempty(folderModelOptions{i_model}))
+            if (iscell(data.modelOptions) && length(data.modelOptions) > 1)
+              [~, dirName] = fileparts(modelFolders{i_model});
+              folderModelOptions{i_model} = getThisModelOption(dirName, data.modelOptions);
+            elseif (isstruct(data.modelOptions))
+              folderModelOptions{i_model} = data.modelOptions;
+            else
+              warning('data.modelOptions are mssing or have wrong format');
+            end
           end
+
           if (any(data_instances))
 
             % save the statistics into respective resulting cell arrays
             RDEs{i_model, i_func, i_dim} = data.stats.rde(data_instances, snapshots);
             MSEs{i_model, i_func, i_dim} = data.stats.mse(data_instances, snapshots);
-            isTrained{i_model, i_func, i_dim} = cellfun( ...
-                @(m) m.isTrained(), data.models(data_instances, snapshots) );
+            if (isfield(data, 'models'))
+              isTrained{i_model, i_func, i_dim} = cellfun( ...
+                  @(m) m.isTrained(), data.models(data_instances, snapshots) );
+            end
 
           else
             warning('There''s no instance with any results for %s_%s, f%d in %dD', ...
@@ -90,14 +106,32 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
   % load('/tmp/stats.mat');
 
   rowsCount = length(modelFolders)*length(snapshots);
-  columnsCount = length(functions)*length(dimensions);
+  % three extra columns for hash, dimension and snapshot, and
+  % 3-times more columns than functions as mean, std and # of sucess is stored
+  columnsCount = 3 + (3*length(functions))*length(dimensions);
 
   valuesRDE = cell(rowsCount, columnsCount);
   valuesMSE = cell(rowsCount, columnsCount);
   colNames  = cell(1, columnsCount);
   rowNames  = cell(1, rowsCount);
 
-  multiFieldNames = getFieldsWithMultiValues(modelOpts);
+  % try to run the script exp/experiments/exp_id.m
+  % and load the multi-valued fields from modelOptions
+  exppath_short = fullfile('exp', 'experiments');
+  expScript = fullfile(exppath_short, [exp_id '.m']);
+  multiFieldNames = {};
+  if (exist(expScript, 'file'))
+    run(expScript);
+    if (exist('modelOptions', 'var') && isstruct(modelOptions))
+      multiFieldNames = getFieldsWithMultiValues(modelOptions);
+    end
+  end
+  % if multi-field modelOptions loading was unsuccessful,
+  % use the modelOpts loaded from some model test results above
+  if (isempty(multiFieldNames))
+    multiFieldNames = sort(fields(folderModelOptions{1}));
+    warning('Unable to load non-factorial design of modelOptions. Using all possible fields (%d) from some modelOptions loaded from test results.', length(multiFieldNames));
+  end
   modelOptsValues = cell(rowsCount, length(multiFieldNames));
 
   row = 0;
@@ -114,20 +148,18 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
 
         row = row + 1;
         rowNames{row} = sprintf('%s_%s_%dD_S%d', modelName, hash, dim, snapshot);
+
+        % save values of model settings (from modelOptions) which had
+        % multiple possible values
         for m_opt = 1:length(multiFieldNames)
-          % modelOptsValues{row} = ...
+          modelOptsValues{row, m_opt} = folderModelOptions{i_model}.(multiFieldNames{m_opt});
         end
-        trainsetType{row} = modelOpts{i_model}.trainsetType;
-        trainsetSizeMax{row} = modelOpts{i_model}.trainsetSizeMax;
-        trainRange{row} = modelOpts{i_model}.trainRange;
-        covFcn{row} = modelOpts{i_model}.covFcn;
-        meanFcn{row} = modelOpts{i_model}.meanFcn;
 
         % save current dimension and snapshot into the first two columns
-        colNames(1:2) = {'dim', 'snapshot'};
-        valuesRDE(row, 1:2) = {dim, snapshot};
-        valuesMSE(row, 1:2) = {dim, snapshot};
-        col = 2;
+        colNames(1:3) = {'hash', 'dim', 'snapshot'};
+        valuesRDE(row, 1:3) = {hash, dim, snapshot};
+        valuesMSE(row, 1:3) = {hash, dim, snapshot};
+        col = 3;
 
         % save statistics from each model / function / dimension / snapshot
         for i_func = 1:length(functions)
@@ -148,30 +180,28 @@ function [rdeTable, mseTable, RDEs, MSEs] = modelStatistics(modelFolders, functi
           % the number of successfuly trained models
           col = col + 1;
           colNames{col} = sprintf('f%d_tr', func);
-          valuesRDE{row, col} = sum(isTrained{i_model, i_func, i_dim}(:, i_snapshot));
-          valuesMSE{row, col} = sum(isTrained{i_model, i_func, i_dim}(:, i_snapshot));
+          if (~isempty(isTrained{i_model, i_func, i_dim}))
+            valuesRDE{row, col} = sum(isTrained{i_model, i_func, i_dim}(:, i_snapshot));
+            valuesMSE{row, col} = sum(isTrained{i_model, i_func, i_dim}(:, i_snapshot));
+          end
         end
       end
     end
   end
 
-  rdeTable = cell2table(valuesRDE, 'VariableNames', colNames);
+  rdeTable = cell2table([modelOptsValues, valuesRDE], 'VariableNames', [multiFieldNames, colNames]);
   rdeTable.Properties.RowNames = rowNames;
-  modelOptsTable = cell2table([trainsetType, trainsetSizeMax, trainRange, covFcn, meanFcn], ...
-    'VariableNames', {'trainsetType', 'trainsetSizeMax', 'trainRange', 'covFcn', 'meanFcn'});
-  rdeTable = [modelOptsTable, rdeTable];
   disp('RDE Table');
   disp(rdeTable);
-  writetable(rdeTable, 'rdeTable.txt', 'WriteRowNames', true);
-  save('RDEs.mat','RDEs');
+  writetable(rdeTable, fullfile(exppath_short, exp_id, 'rdeTable.txt'), 'WriteRowNames', true);
 
-  mseTable = cell2table(valuesMSE, 'VariableNames', colNames);
+  mseTable = cell2table([modelOptsValues, valuesMSE], 'VariableNames', [multiFieldNames, colNames]);
   mseTable.Properties.RowNames = rowNames;
-  mseTable = [modelOptsTable, mseTable];
   disp('MSE Table');
   disp(mseTable);
-  writetable(mseTable, 'mseTable.txt', 'WriteRowNames', true);
-  save('MSEs.mat','MSEs');
+  writetable(mseTable, fullfile(exppath_short, exp_id, 'mseTable.txt'), 'WriteRowNames', true);
+
+  save(fullfile(exppath_short, exp_id, 'modelStatistics.mat'), 'rdeTable', 'mseTable', 'RDEs', 'valuesRDE', 'MSEs', 'valuesMSE', 'isTrained', 'folderModelOptions', 'modelFolders', 'functions', 'dimensions', 'snapshots');
 end
 
 function [modelName, hash, FEs] = parseFolderName(dirName)
@@ -192,13 +222,32 @@ end
 function multiFieldNames = getFieldsWithMultiValues(modelOpts)
 % Get names of fields with more than one value in its cell-array fieldvalue
   assert(isstruct(modelOpts));
-  multiFieldNames = {};
 
   fnames = fieldnames(modelOpts);
+  % multiFieldNames = cell(1, length(fnames));
+  multiFieldNames = {};
   for i = 1:length(fnames)
     fname = fnames{i};
     if (iscell(modelOpts.(fname)) && length(modelOpts.(fname)) > 1)
-      multiFieldNames{i} = fname;
+      multiFieldNames{end+1} = fname;
     end
   end
+end
+
+function [mo_struct, mo_idx] = getThisModelOption(dirName, modelOptions)
+  m = regexp(dirName, '_([0-9]+)_', 'tokens');
+  if (~isempty(m{1}{1}))
+    dirHash = str2double(m{1}{1});
+
+    for i = 1:length(modelOptions)
+      if (str2double(modelHash(modelOptions{i})) == dirHash)
+        mo_idx = i;
+        mo_struct = modelOptions{i};
+        return;
+      end
+    end
+  end
+  fprintf(2, 'The right index of the directory hash not found in modelOptions.\n');
+  mo_idx = -1;
+  mo_struct = [];
 end
