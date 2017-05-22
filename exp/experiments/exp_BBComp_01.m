41de7183197c361e0da6b807a019292b714ec7e3
Lukas Bajer <bajeluk@seznam.cz>
2017-05-22 16:22:36 +0200

    [BBCOMP2017] Metacentrum basic script for MCR compile

---
+++ exp/experiments/exp_BBComp_01.m
@@ -0,0 +1,50 @@
+exp_id = 'exp_BBComp_01';
+exp_description = 'Surrogate CMA-ES settings for BBComp 1-OBJ task, DTS with preselection(1pt), DTIterations={2}, Valid.Gen.Period/PopSz=4/2, PreSampleSize=0.75, PoI criterion, 2pop';
+
+% Surrogate manager parameters
+
+surrogateParams.evoControl = 'doubletrained';
+surrogateParams.observers = {'DTScreenStatistics', 'DTFileStatistics'};
+surrogateParams.modelType = 'gp';
+surrogateParams.evoControlRestrictedParam = 0.05;
+surrogateParams.evoControlTrainRange = 10;             % will be multip. by sigma
+surrogateParams.evoControlTrainNArchivePoints = '15*dim'; % will be myeval()'ed, 'nRequired', 'nEvaluated', 'lambda', 'dim' can be used
+
+surrogateParams.updaterType = 'rankDiff';
+surrogateParams.DTAdaptive_updateRate = 0.3;
+surrogateParams.DTAdaptive_updateRateDown = 0.6;
+surrogateParams.DTAdaptive_maxRatio = 0.9;
+surrogateParams.DTAdaptive_minRatio = 0.04;
+surrogateParams.DTAdaptive_lowErr = 0.15;
+surrogateParams.DTAdaptive_highErr = 0.40;
+surrogateParams.DTAdaptive_defaultErr = 0.10;
+
+surrogateParams.evoControlMaxDoubleTrainIterations = 2;
+surrogateParams.evoControlPreSampleSize = 0.75;
+surrogateParams.evoControlNBestPoints = [0.2 1.0];
+surrogateParams.evoControlValidationGenerationPeriod = 4;
+surrogateParams.evoControlValidationPopSize = 2;
+surrogateParams.evoControlOrigPointsRoundFcn = 'ceil'; % 'ceil', 'getProbNumber'
+
+surrogateParams.evoControlIndividualExtension = [];    % will be multip. by lambda
+surrogateParams.evoControlBestFromExtension = [];      % ratio of expanded popul.
+surrogateParams.evoControlSampleRange = 1;             % will be multip. by sigma
+surrogateParams.evoControlValidatePoints = [];
+
+% Model parameters
+
+surrogateParams.modelOpts.useShift        = false;
+surrogateParams.modelOpts.predictionType  = 'poi';
+surrogateParams.modelOpts.trainAlgorithm  = 'fmincon';
+surrogateParams.modelOpts.covFcn          = '{@covMaterniso, 5}';
+surrogateParams.modelOpts.normalizeY      = true;
+surrogateParams.modelOpts.hyp.lik         = log(0.01);
+surrogateParams.modelOpts.hyp.cov         = log([0.5; 2]);
+surrogateParams.modelOpts.covBounds       = [ [-2;-2], [25;25] ];
+surrogateParams.modelOpts.likBounds       = log([1e-6, 10]);
+
+% CMA-ES parameters
+
+cmaesParams.PopSize = '(8 + floor(6*log(N)))';
+cmaesParams.Restarts = 4;
+cmaesParams.DispModulo = 0;
---
+++ exp/metacentrum_bbcomp_task.m
@@ -0,0 +1,107 @@
+function status = metacentrum_bbcomp_task(exp_id, exppath_short, problemID_str, opts_str)
+% metacentrum_bbcomp_task -- Matlab part of BBComp 2017 competition scripts to be MCR-compiled
+%
+% Usage:
+%   metacentrum_bbcomp_task(exp_id, exppath_short, problemID_str, opts_str)
+
+  % Input parameter settings
+  %
+  % ID/opts input parse settings
+  if (~exist('problemID_str', 'var'))
+    problemID_str = []; end
+  if (~exist('opts_str', 'var'))
+    opts_str = []; end
+
+  id            = parseCmdParam('problemID_str', problemID_str, 1:5);
+  cmd_opts      = parseCmdParam('opts_str', opts_str, struct());
+
+  % run the script EXP_ID.m if it exists
+  opts = struct();
+  expScript = fullfile(exppath_short, [exp_id '.m']);
+  if (exist(expScript, 'file'))
+    % eval the script line by line (as it cannot be run()-ed when deployed)
+    fid = fopen(expScript);
+    tline = fgetl(fid);
+    while (ischar(tline))
+      eval(tline);
+      tline = fgetl(fid);
+    end
+    fclose(fid);
+  end
+  % and re-write the options specified on command-line
+  if (isstruct(cmd_opts) && ~isempty(cmd_opts))
+    cmd_fnames = fieldnames(cmd_opts);
+    for i = 1:length(cmd_fnames)
+      opts.(cmd_fnames{i}) = cmd_opts.(cmd_fnames{i});
+    end
+  end
+
+  % EXPID -- unique experiment identifier (directory in where 
+  %          the results are expected to be placed)
+  opts.exp_id     = exp_id;
+  % EXPPATH_SHORT
+  opts.exppath_short = exppath_short;
+  opts.exppath = [exppath_short filesep exp_id];
+  RESULTSFILE = [opts.exppath '/' exp_id '_results_' num2str(id) '.mat'];
+  if (isempty(getenv('SCRATCHDIR')))
+    OUTPUTDIR = '/tmp/job_output';     % set OUTPUTDIR empty if $SCRATCHDIR var does not exist
+  else
+    OUTPUTDIR = [getenv('SCRATCHDIR') filesep 'job_output'];
+  end
+
+  % datapath is typically on the computing node in   $SCRATCHDIR/job_output/bbcomp_output:
+  datapath = [OUTPUTDIR filesep 'bbcomp_output'];
+  if (isempty(strfind(datapath, exppath_short)))
+    % localDatapath is typically on the NFS server in
+    %   $HOME/prg/surrogate-cmaes/exp/experiments/$EXPID/bbcomp_output_tmp
+    % and the bbcomp results are stored here after each completed instance
+    localDatapath = [opts.exppath filesep 'bbcomp_output_tmp'];
+    [~, ~] = mkdir(localDatapath);
+  end
+  [~, ~] = mkdir(datapath);
+
+  % Metacentrum task and node properties
+  cmd_opts.machine = '';
+  nodeFile = fopen(getenv('PBS_NODEFILE'), 'r');
+  if (nodeFile > 0)
+    cmd_opts.machine = fgetl(nodeFile);
+    fclose(nodeFile);
+  end
+
+  fprintf('== Summary of the testing assignment ==\n');
+  fprintf('   problemID:    %s\n', num2str(id));
+  fprintf('=======================================\n');
+  % Matlab should have been called from a SCRACHDIR
+  startup;
+
+  t0 = clock;
+  % Initialize random number generator
+  exp_settings.seed = myeval(defopts(cmd_opts, 'seed', 'floor(sum(100 * clock()))'));
+  rng(exp_settings.seed);
+
+  %
+  %
+  % the computation itself
+  %
+  %
+
+
+  % copy the bbcomp results onto persistant storage if outside EXPPATH
+  if (~isempty(OUTPUTDIR) && ~strcmpi(OUTPUTDIR, opts.exppath) && isunix)
+    % copy the output to the final storage (if OUTPUTDIR and EXPPATH differs)
+    system(['cp -pR ' OUTPUTDIR '/* ' opts.exppath '/']);
+  end
+
+  status = 0;
+  return;
+end
+
+function out = parseCmdParam(name, value, defaultValue)
+  if (isempty(value))
+    out = defaultValue;
+  elseif (ischar(value))
+    out = myeval(value);
+  else
+    error('%s has to be string for eval()-uation', name);
+  end
+end
