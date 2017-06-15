7a09e6901a1fe529ed8ea06a8492941dea9daccd
Zbynek Pitra <jdgregorian@seznam.cz>
2017-06-15 13:39:42 +0200

    [ITAT 2017] Parameter settings added

--- exp/pproc/scripts/generatePlots_itat2017.m
+++ exp/pproc/scripts/generatePlots_itat2017.m
@@ -32,15 +32,19 @@ end
 % path settings
 exppath = fullfile('exp', 'experiments');
 
-adts_path = fullfile(exppath, 'DTS-CMA-ES_05_2pop');
-dts_path = fullfile(exppath, 'exp_doubleEC_23');
+adts_he50_path      = fullfile(exppath, 'exp_doubleEC_23_adapt05_v3');
+adts_le50_he75_path = fullfile(exppath, 'exp_doubleEC_23_adapt05_v2');
+adts_le75_he75_path = fullfile(exppath, 'exp_doubleEC_23_adapt05');
 
+dts_path = fullfile(exppath, 'exp_doubleEC_23');
 cmaes_path = fullfile(exppath, 'CMA-ES');
 saacmes_path = fullfile(exppath, 'BIPOP-saACM-k');
 lmm_path = fullfile(exppath, 'lmm-CMA-ES');
 
 % load data
-dataFolders = {adts_path; ...
+dataFolders = {adts_he50_path; ...
+               adts_le50_he75_path; ...
+               adts_le75_he75_path; ...
                dts_path; ...
                cmaes_path; ...
                saacmes_path; ...
@@ -51,12 +55,61 @@ dataFolders = {adts_path; ...
 % find ids in settings
 
 % adaptive DTS settings id
-
 clear findSet
-findSet.evoControlRestrictedParam = 0.05;
-findSet.modelOpts.predictionType = 'sd2';
+% quantiles: lowErr 0.5, highErr 0.5
+findSet.DTAdaptive_lowErr = 0.5;
+findSet.DTAdaptive_highErr = 0.5;
+findSet.DTAdaptive_updateRate = 0.3;
+adts_le50_he50_ur3_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.4;
+adts_le50_he50_ur4_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.5;
+adts_le50_he50_ur5_Id = getStructIndex(settings, findSet);
+
+% quantiles: lowErr 0.5, highErr 0.75
+findSet.DTAdaptive_lowErr = 0.5;
+findSet.DTAdaptive_highErr = 0.75;
+findSet.DTAdaptive_updateRate = 0.3;
+adts_le50_he75_ur3_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.4;
+adts_le50_he75_ur4_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.5;
+adts_le50_he75_ur5_Id = getStructIndex(settings, findSet);
+
+% quantiles: lowErr 0.75, highErr 0.5
+findSet.DTAdaptive_lowErr = 0.75;
+findSet.DTAdaptive_highErr = 0.5;
+findSet.DTAdaptive_updateRate = 0.3;
+adts_le75_he50_ur3_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.4;
+adts_le75_he50_ur4_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.5;
+adts_le75_he50_ur5_Id = getStructIndex(settings, findSet);
+
+% quantiles: lowErr 0.75, highErr 0.75
+findSet.DTAdaptive_lowErr = 0.75;
+findSet.DTAdaptive_highErr = 0.75;
+findSet.DTAdaptive_updateRate = 0.3;
+adts_le75_he75_ur3_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.4;
+adts_le75_he75_ur4_Id = getStructIndex(settings, findSet);
+
+findSet.DTAdaptive_updateRate = 0.5;
+adts_le75_he75_ur5_Id = getStructIndex(settings, findSet);
+
+% DTS settings id
+clear findSet
+findSet.DTAdaptive_defaultErr = [];
 dts_Id = getStructIndex(settings, findSet);
 
+% CMA-ES, saACMES, lmm-CMA-ES settings id
 clear findSet
 findSet.algName = 'CMA-ES';
 cma_Id = getStructIndex(settings, findSet);
@@ -71,11 +124,43 @@ saacmes_data   = evals(:, :, saacm_Id);
 dtscmaes_data  = evals(:, :, dts_Id);
 lmmcmaes_data  = evals(:, :, lmm_Id);
 
+adts_le50_he50_ur3_data = evals(:, :, adts_le50_he50_ur3_Id);
+adts_le50_he50_ur4_data = evals(:, :, adts_le50_he50_ur4_Id);
+adts_le50_he50_ur5_data = evals(:, :, adts_le50_he50_ur5_Id);
+
+adts_le50_he75_ur3_data = evals(:, :, adts_le50_he75_ur3_Id);
+adts_le50_he75_ur4_data = evals(:, :, adts_le50_he75_ur4_Id);
+adts_le50_he75_ur5_data = evals(:, :, adts_le50_he75_ur5_Id);
+
+adts_le75_he50_ur3_data = evals(:, :, adts_le75_he50_ur3_Id);
+adts_le75_he50_ur4_data = evals(:, :, adts_le75_he50_ur4_Id);
+adts_le75_he50_ur5_data = evals(:, :, adts_le75_he50_ur5_Id);
+
+adts_le75_he75_ur3_data = evals(:, :, adts_le75_he75_ur3_Id);
+adts_le75_he75_ur4_data = evals(:, :, adts_le75_he75_ur4_Id);
+adts_le75_he75_ur5_data = evals(:, :, adts_le75_he75_ur5_Id);
+
 % color settings
 cmaesCol     = getAlgColors('cmaes');
 saacmesCol   = getAlgColors('saacmes');
 dtsCol       = getAlgColors('dtscmaes');
 lmmCol       = getAlgColors('lmmcmaes');
+
+adts_le50_he50_ur3_col = getAlgColors(1);
+adts_le50_he50_ur4_col = getAlgColors(2);
+adts_le50_he50_ur5_col = getAlgColors(3);
+
+adts_le50_he75_ur3_col = getAlgColors(4);
+adts_le50_he75_ur4_col = getAlgColors(5);
+adts_le50_he75_ur5_col = getAlgColors(6);
+
+adts_le75_he50_ur3_col = getAlgColors(7);
+adts_le75_he50_ur4_col = getAlgColors(8);
+adts_le75_he50_ur5_col = getAlgColors(9);
+
+adts_le75_he75_ur3_col = getAlgColors(10);
+adts_le75_he75_ur4_col = getAlgColors(11);
+adts_le75_he75_ur5_col = getAlgColors(12);
 
 % aggregate data & settings
 data = {cmaes_data, ...
@@ -92,6 +177,37 @@ if (~exist(tmpFName, 'file'))
 end
 
 end
+
+%% ADTS-CMA-ES settings comparison: lowErr, highErr, updateRate
+% Aggregation of function values across dimensions 2, 3, 5, 10, 20.
+
+close all
+
+set_data = {adts_le50_he50_ur3_data, ...
+            adts_le50_he50_ur4_data, ...
+            adts_le50_he50_ur5_data, ...
+            adts_le50_he75_ur3_data, ...
+            adts_le50_he75_ur4_data, ...
+            adts_le50_he75_ur5_data, ...
+            adts_le75_he50_ur3_data, ...
+            adts_le75_he50_ur4_data, ...
+            adts_le75_he50_ur5_data, ...
+            adts_le75_he75_ur3_data, ...
+            adts_le75_he75_ur4_data, ...
+            adts_le75_he75_ur5_data};
+
+set_datanames = {'$\\err_\\text{min}$, $\\err_\\text{max}$'};
+
+tableFunc = funcSet.BBfunc;
+tableDims = funcSet.dims;
+
+resultTable = fullfile(tableFolder, 'rankTable.tex');
+      
+[table, ranks] = rankingTable(set_data, 'DataNames', set_datanames, ...
+                           'DataFuns', funcSet.BBfunc, 'DataDims', funcSet.dims, ...
+                           'TableFuns', tableFunc, 'TableDims', tableDims,...
+                           'Evaluations', [20 40 80], ...
+                           'ResultFile', resultTable);
 
 %% Algorithm comparison: CMA-ES, lmm-CMA-ES, saACMES, DTS-CMA-ES  
 % Scaled function values of f1-f24 in dimension 5.
