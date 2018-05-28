85492bb1b927e03900f2c854f2c989a990d9d3ec
repjak <j.repicky@gmail.com>
2018-05-28 20:47:39 +0200

    Provisional fix for struct arrays.

--- exp/util/sprintfStruct.m
+++ exp/util/sprintfStruct.m
@@ -1,27 +1,36 @@
 function output = sprintfStruct(s, varargin)
   output = [];
-  for fname = fieldnames(s)'
-    empty = true;
-    if (isnumeric(s.(fname{1})))
-      str = num2str(s.(fname{1}));
-    elseif (isstr(s.(fname{1})))
-      str = s.(fname{1});
-    elseif (islogical(s.(fname{1})))
-      if (s.(fname{1}))
-        str = 'true';
-      else
-        str = 'false';
-      end
-    elseif (isstruct(s.(fname{1})) && ~strcmpi(fname{1}, 'parameterSets'))
-      str = sprintfStruct(s.(fname{1}), 'escape');
+  if length(s) > 1
+    for i = 1:length(s)
+      output = [output '\\n' sprintfStruct(s(i), 'escape')];
     end
-    if (~isempty(str))
-      if (nargin > 1 && strcmpi(varargin{1}, 'escape'))
-        backN = '\\\\\\n';
-      else
-        backN = '\\n';
+  else
+    for fname = fieldnames(s)'
+      empty = true;
+      if (isnumeric(s.(fname{1})))
+        str = num2str(s.(fname{1}));
+      elseif (isstr(s.(fname{1})))
+        str = s.(fname{1});
+      elseif (islogical(s.(fname{1})))
+        if (s.(fname{1}))
+          str = 'true';
+        else
+          str = 'false';
+        end
+      elseif (isstruct(s.(fname{1})) && ~strcmpi(fname{1}, 'parameterSets'))
+        str = sprintfStruct(s.(fname{1}), 'escape');
+      elseif (iscell(s.(fname{1})))
+        c = s.(fname{1});
+        str = sprintf('%s ', c{:});
       end
-      output = [output sprintf(['%15s: %s' backN], fname{1}, str)];
+      if (~isempty(str))
+        if (nargin > 1 && strcmpi(varargin{1}, 'escape'))
+          backN = '\\\\\\n';
+        else
+          backN = '\\n';
+        end
+        output = [output sprintf(['%15s: %s' backN], fname{1}, str)];
+      end
     end
   end
 end
