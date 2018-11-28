6ceeb8d00b7d780879fc5e34c53f5cbb8964beb1
repjak <j.repicky@gmail.com>
2018-11-28 10:11:58 +0100

    Reliable detection of Q|R states (using qstat). Add detection of error msgs on stdout.

--- exp/metacentrum_list.sh
+++ exp/metacentrum_list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # metacentrum_list -crqn [EXPID] [ID1] [ID2]
-# 
+#
 
 # Help function
 function HELP {
@@ -11,16 +11,30 @@ function HELP {
   NORM=`tput sgr0`
   BOLD=`tput bold`
   REV=`tput smso`
-  echo -e "\nUsage:\n$SCRIPT -crqn [EXPID] [ID1] [ID2] ...\n"
+  echo -e "\nUsage:\n$SCRIPT -ecrqn [EXPID] [ID1] [ID2] ...\n"
   echo "where [ID*] are the experiment ID# (accessible from allids.txt)"
   echo "Command line switches are optional. The following switches are recognized."
   echo "  ${REV}-c${NORM}  --List completed tasks."
   echo "  ${REV}-r${NORM}  --List running tasks."
   echo "  ${REV}-q${NORM}  --List queued tasks."
   echo "  ${REV}-n${NORM}  --List not completed, running or queued tasks."
+  echo "  ${REV}-e${NORM}  --Tasks that are not running or queued according to qstat, but have no FINISHED message in their stdout."
   echo -e "  ${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
   echo -e "Example: ${BOLD}$SCRIPT -cr exp_CMA-ES_01 {100..200}${NORM}"\\n
   exit 1
+}
+
+# Filter output of qstat in json format using jq parser
+function QSTAT_FILTER {
+  # jq (json parser) executable
+  local JQ_PATH=$CWD/vendor/jq-1.6/jq
+  # path to qstat output
+  local QSTAT_FILE=$1
+  local EXPID=$2
+  local JOB_STATE=$3
+
+  # filter jobs by names matching a regexp and state codes equal to a string; output numerical job suffix
+  $JQ_PATH ".Jobs[] | select((.Job_Name | tostring | test(\"^${EXPID}__[0-9]+$\")) and .job_state == \"${JOB_STATE}\") | .Job_Name | sub(\"${EXPID}__\"; \"\")" $QSTAT_FILE;
 }
 
 if [ "$#" -eq 0 ]; then
@@ -32,13 +46,14 @@ COMPLETED=0
 RUNNING=0
 QUEUED=0
 NOSTATE=0
+ERRORED=0
 
 #TODO: no options - default : crqn
 
-while getopts "crqnh:" FLAG; do
+while getopts "ecrqnh:" FLAG; do
   case $FLAG in
     c)  #set option "c"
-      COMPLETED=1 
+      COMPLETED=1
       ;;
     r)  #set option "r"
       RUNNING=1
@@ -48,6 +63,9 @@ while getopts "crqnh:" FLAG; do
       ;;
     n)  #set option "n"
       NOSTATE=1
+      ;;
+    e)  #set option "e"
+      ERRORED=1
       ;;
     h)  #show help
       HELP
@@ -72,6 +90,8 @@ cd $CWD/experiments/$EXPID
 
 ls -1 ${EXPID}_results_*[0-9].mat calculating_* queued_* > $TMPFILE
 
+QSTAT_OUT=""
+
 # completed
 if [ $COMPLETED -eq 1 ]; then
   echo "Completed:"
@@ -86,9 +106,15 @@ fi
 
 # running
 if [ $RUNNING -eq 1 ]; then
+  if [ -n $QSTAT_OUT ]; then
+    QSTAT_OUT=`tempfile`.json
+    qstat -f -F json > $QSTAT_OUT
+  fi
+
   echo "Running:"
+  LIST=`QSTAT_FILTER $QSTAT_OUT $EXPID R`
   for i in $IDS; do
-    if grep -q "calculating_${i}$" $TMPFILE; then
+    if echo $LIST | grep -q "\"${i}\""; then
       echo -n "$i "
     fi
   done
@@ -97,14 +123,54 @@ fi
 
 # queued
 if [ $QUEUED -eq 1 ]; then
+  if [ -n $QSTAT_OUT ]; then
+    QSTAT_OUT=`tempfile`.json
+    qstat -f -F json > $QSTAT_OUT
+  fi
+
   echo "Queued (removing queued files is not reliable):"
+  LIST=`QSTAT_FILTER $QSTAT_OUT $EXPID Q`
   for i in $IDS; do
-    if grep -q "queued_${i}$" $TMPFILE; then
+    if echo $LIST | grep -q "\"${i}\""; then
       echo -n "$i "
     fi
   done
   echo ""
 fi
+
+# errored out
+if [ $ERRORED -eq 1 ]; then
+  echo $CWD/..
+  if [ -n $QSTAT_OUT ]; then
+    QSTAT_OUT=`tempfile`.json
+    qstat -f -F json > $QSTAT_OUT
+  fi
+
+  echo "Errored out:"
+  Q_LIST=`QSTAT_FILTER $QSTAT_OUT $EXPID Q`
+  R_LIST=`QSTAT_FILTER $QSTAT_OUT $EXPID R`
+  for i in $IDS; do
+    echo $Q_LIST | grep -q "\"${i}\"" || echo $R_LIST | grep -q "\"${i}\""
+    if [ $? -eq 0 ]; then
+      # job running or queued, skip
+      continue;
+    fi
+
+    OUTS=`find ${CWD}/.. -maxdepth 1 -name "${EXPID}__${i}.o*"`
+    if [ -z "$OUTS" ]; then
+      # outputs not present
+      continue;
+    fi
+
+    grep -q FINISHED $OUTS
+    if [ $? -ne 0 ]; then
+      echo -n "${i} ";
+    fi
+  done
+  echo ""
+fi
+
+
 
 # no state
 if [ $NOSTATE -eq 1 ]; then
