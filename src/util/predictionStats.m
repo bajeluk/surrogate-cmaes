b86bd1d0c57a59a1b28a196447c95d4b900f224d
repjak <j.repicky@gmail.com>
2018-12-06 10:19:01 +0100

    Add mae and r2 prediction statistics.

--- src/util/predictionStats.m
+++ src/util/predictionStats.m
@@ -11,14 +11,17 @@ function s = predictionStats(y1, y2, stat)
 %            rankmse  - mean square error of ranks
 %            rankmzoe - mean zero-one error of ranks
 %            rde      - ranking difference error
+%            mae      - mean absolute error
+%            r2       - r squared, aka explained variance, aka coeff. of
+%                       determination
 
   % normalize input
   y1 = y1(:);
   y2 = y2(:);
-  
+
   n = numel(y1);
   assert(numel(y2) == n, 'Lengths of compared vectors has to be equal.')
-  
+
   % compute ranking statistics
   if length(stat) > 3 && strcmp(stat(1:4), 'rank')
     [~, id] = sort(y1);
@@ -37,6 +40,15 @@ function s = predictionStats(y1, y2, stat)
       s = corr(y2, y1, 'type', 'kendall');
     case 'rde'
       s = errRankMu(y2, y1, floor(n/2));
+    case 'mae'
+      s = sum(abs(y2 - y1)) / n;
+    case 'r2'
+      y1_mean = mean(y1);
+      % regression sum of squares
+      ss_reg = sum((y2 - y1_mean).^2);
+      % total sum of squares
+      ss_tot = sum((y1 - y1_mean).^2);
+      s = ss_reg / ss_tot;
     otherwise
       s = NaN;
   end
