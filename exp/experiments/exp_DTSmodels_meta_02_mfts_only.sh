53751208b3958e5620ab616e2d3d1308118d311c
Jakub Repicky <j.repicky@gmail.com>
2019-01-16 00:13:10 +0100

    Metalearning scripts:
    * failed - resubmitting failed ids
    * mfts_only - only metafeature calculation, model training omitted
    * new_data - rerun model testing when new DTS runs have finished

---
+++ exp/experiments/exp_DTSmodels_meta_02_mfts_only.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Job-submitting experiment for testing multiple gp models
+# on snapshots generated from multiple DTS experiments. 
+
+export EXPID='exp_DTSmodels_meta_02'
+export DATASET="exp/experiments/dataset/DTS_meta_005.mat"
+
+# Enable this option for using Matlab MCR compilated binaries:
+export useMCR=1
+
+# !!! THIS HAS TO BE HERE !!!
+#     vvvvvvvvvvvvvvvvvvv
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. $CWD/../bash_settings.sh
+. $CWD/../metacentrum_testmodels_common.sh
+#     ^^^^^^^^^^^^^^^^^^^
+# !!! THIS HAS TO BE HERE !!!
+
+# job submittion can be done via one of the two bash functions:
+#
+# (a) subtask [JOBNAME_SUFFIX]
+#
+#     submits a job with current-set $ID, $DIM, $FUNC, $INST and $OPTS
+#
+#
+# (b) submit_sequence LOW_IDX STEP HIGH_IDX
+#
+#     call subtask() with 'modelOptionsIndices' set to a small
+#     numbers of model-settings;
+#     it submits a job for every $2 consecutive modelOptions' indices
+#
+#     e.g.: submit 11 4 22
+#     submits jobs for the following modelOption indices:
+#       - 11:14
+#       - 15:18
+#       - 19:22
+#
+#     Important: the HIGH_IDX should be the equal to (k*LOW_IDX) - 1
+#                for some k
+
+
+# critical characters has to be replaced in $OPTS:
+# '|' with ',' and "%" with "'"
+
+#for i in {1..6} {8..9}; do
+#  OPTS[${#OPTS[@]}]="struct(%modelOptionsIndices%| $i)"
+#done
+
+INST="[11 12 13 14 15]"
+IDS="[1 2 3 4 5 6 8 9]" # 7 is ADD kernel
+
+ID=3001
+OPTS="struct(%modelOptionsIndices%|[1 2 3 4 5 6 8 9]|%mfts_only%|1|%rewrite_results%|1)"
+
+for DIM in 2 3 5 10 20; do
+  case $DIM in
+    [23])
+      QUEUE="4:00:00"
+      ;;
+    *)
+      QUEUE="24:00:00"
+      ;;
+  esac
+
+  for FUNC in `seq 1 24`; do
+    subtask
+  done
+done
---
+++ exp/experiments/exp_DTSmodels_meta_02_new_data.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Job-submitting experiment for the first 10D GP model testing
+# It is expected to be located in exp/experiments/
+
+export EXPID='exp_DTSmodels_meta_02'
+export DATASET="exp/experiments/dataset/DTS_meta_005.mat"
+
+# Enable this option for using Matlab MCR compilated binaries:
+export useMCR=1
+
+# !!! THIS HAS TO BE HERE !!!
+#     vvvvvvvvvvvvvvvvvvv
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. $CWD/../bash_settings.sh
+. $CWD/../metacentrum_testmodels_common.sh
+#     ^^^^^^^^^^^^^^^^^^^
+# !!! THIS HAS TO BE HERE !!!
+
+# job submittion can be done via one of the two bash functions:
+#
+# (a) subtask [JOBNAME_SUFFIX]
+#
+#     submits a job with current-set $ID, $DIM, $FUNC, $INST and $OPTS
+#
+#
+# (b) submit_sequence LOW_IDX STEP HIGH_IDX
+#
+#     call subtask() with 'modelOptionsIndices' set to a small
+#     numbers of model-settings;
+#     it submits a job for every $2 consecutive modelOptions' indices
+#
+#     e.g.: submit 11 4 22
+#     submits jobs for the following modelOption indices:
+#       - 11:14
+#       - 15:18
+#       - 19:22
+#
+#     Important: the HIGH_IDX should be the equal to (k*LOW_IDX) - 1
+#                for some k
+
+
+# critical characters has to be replaced in $OPTS:
+# '|' with ',' and "%" with "'"
+MODEL_IDS="1 2 3 4 5 6 8 9"
+
+INST="[11 12 13 14 15]"
+IDS="[1 2 3 4 5 6 8 9]" # 7 is ADD kernel
+#FAILED_LINE=`exp/metacentrum_list.sh -e $EXPID {1..960} | grep -A1 "Errored out" | tail -n1`
+#read -a FAILED <<< $FAILED_LINE
+MISSING=("f1_10D" "f1_20D" "f2_20D" "f3_20D" "f4_20D" "f5_20D" "f6_10D" "f6_20D" "f7_10D" "f7_20D" "f8_10D" "f8_20D" "f9_10D" "f9_20D")
+#if [[ ${#FAILED[@]} -eq 0 ]]; then
+#  echo "No failed ids found."
+#  exit
+#fi
+#
+#echo -n "Submit ${#FAILED[@]} failed ids (${FAILED[@]})? [y/N] "
+#read ANS
+
+#if [[ $ANS = [yY] ]]; then
+#  echo "Submitting..."
+#  sleep 1
+#else
+#  exit
+#fi
+
+ID=1
+
+for DIM in 2 3 5 10 20; do
+  case $DIM in
+    [235])
+      QUEUE="168:00:00"
+      ;;
+    10)
+      QUEUE="168:00:00"
+      ;;
+    *)
+      QUEUE="168:00:00"
+      ;;
+  esac
+
+  for FUNC in `seq 1 24`; do
+    for MODEL_ID in $MODEL_IDS; do
+      OPTS="struct(%modelOptionsIndices%|$MODEL_ID)"
+
+      #echo "${FAILED[@]}" | grep "\b$ID\b" > /dev/null
+      echo "${MISSING[@]}" | grep "\bf${FUNC}_${DIM}D\b" > /dev/null
+      if [[ $? -eq 0 ]]; then
+        subtask
+      else
+        ID=$((ID+1))
+      fi
+    done
+  done
+done
