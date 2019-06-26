1c513a87cb23e6869d65ac0d8269b440e813c123
lubajer <lubajer@cisco.com>
2019-06-27 01:39:30 +0200

    More statistics for LogParser

--- exp/pproc/python/covariance_log_parser/covparser/covparser.py
+++ exp/pproc/python/covariance_log_parser/covparser/covparser.py
@@ -22,6 +22,9 @@ class CovarianceFunction:
     def __init__(self):
         self.cov_strings_dict = {self.cov_strings_array[i]: i for i in range(len(self.cov_strings_array))}
 
+    def fun_count(self):
+        return len(self.cov_strings_array)
+
     def to_int(self, name):
         return self.cov_strings_dict[name]
 
@@ -38,11 +41,15 @@ class Run:
         self.second_covariances = []
         self.evaluations = []
         self.length = 0
+        self.cov = CovarianceFunction()
 
-    def setup(self, func, dimension, instance):
+    def setup(self, func, dimension, instance, evaluations, dopt, time):
         self.function = func
         self.dimension = dimension
         self.instance = instance
+        self.evaluations_count = evaluations
+        self.dopt = dopt
+        self.time = time
 
     def insert_generation(self, first_covariance, second_covariance, evaluations):
         self.first_covariances.append(first_covariance)
@@ -58,11 +65,30 @@ class Run:
         else:
             return -1
 
+    def histogram(self, cov_array):
+        d = {}
+        for c in cov_array:
+            if c not in d:
+                d[c] = 0
+            d[c] += 1
+        for i in range(self.cov.fun_count()):
+            if i not in d:
+                d[i] = 0
+        return [d[cov] for cov in range(self.cov.fun_count())]
+
     def to_json(self):
         res = {
             "function": self.function,
             "dimension": self.dimension,
             "instance": self.instance,
+            "evaluationsCount": self.evaluations_count,
+            "generationsCount": len(self.first_covariances),
+            "distanceOptimum": self.dopt,
+            "time": self.time,
+            "distinctFirstCovariances": len(set(self.first_covariances).difference([-1])),
+            "distinctSecondCovariances": len(set(self.second_covariances).difference([-1])),
+            "firstCovariancesHistogram": self.histogram(self.first_covariances),
+            "secondCovariancesHistogram": self.histogram(self.second_covariances),
             "firstCovariances": self.first_covariances,
             "secondCovariances": self.second_covariances,
             "evaluations": self.evaluations
@@ -73,7 +99,9 @@ class Run:
 class RunEntryMatcher:
     def __init__(self):
         #   f3 in 20-D, instance 90: FEs=5008 with 0 restarts, fbest-ftarget=5.7707e+01, elapsed time [h]: 105.65
-        self.final_line_matcher = re.compile(r"^  f([0-9]+) in ([0-9]+)-D, instance ([0-9]+)")
+        self.final_line_matcher = re.compile(r"^  f([0-9]+) in ([0-9]+)-D, instance ([0-9]+): "
+                                             "FEs=([0-9]+) with.*-ftarget=([-+0-9.eE]+), "
+                                             "elapsed time \[h\]: ([-+0-9.eE]+)")
         # Covariance function: @covRQiso
         self.covariance_matcher = re.compile(r"^Covariance function: (.*)")
         # =[DTS]=   99 / 4957(10:0,0) | 6.6e+01 | 6.9e+00 | 0.02 | 0.09 | 0.38 [ *     ] | .  0/371 | 8.89e+00 | 0.00 | 0.00 | 0.05 | 0.00 |
@@ -116,7 +144,8 @@ class RunEntryMatcher:
     def setup_from_final_line(self, line):
         #   f3 in 20-D, instance 90: FEs=5008 with 0 restarts, fbest-ftarget=5.7707e+01, elapsed time [h]: 105.65
         match = self.final_line_matcher.match(line)
-        self.run.setup(int(match.group(1)), int(match.group(2)), int(match.group(3)))
+        self.run.setup(int(match.group(1)), int(match.group(2)), int(match.group(3)),
+                       int(match.group(4)), float(match.group(5)), float(match.group(6)))
 
 
 class LogParser:
@@ -139,6 +168,7 @@ class LogParser:
 
                 if self.generation_line_matcher.match(line):
                     matcher.match_generation_line(line)
+                    matcher.init_generation()
                     # print(line, flush=True)
 
                 if self.final_line_matcher.match(line):
--- exp/pproc/python/covariance_log_parser/tests/test_covparser.py
+++ exp/pproc/python/covariance_log_parser/tests/test_covparser.py
@@ -28,11 +28,14 @@ class TestRun(TestCase):
         assert self.run.length == 0
 
     def test_initialize(self):
-        self.run.setup(1, 2, 3)
+        self.run.setup(1, 2, 3, 10, 1.5e6, 0.23)
         assert self.run.first_covariances == []
         assert self.run.function == 1
         assert self.run.dimension == 2
         assert self.run.instance == 3
+        assert self.run.evaluations_count == 10
+        assert self.run.dopt == 1.5e6
+        assert self.run.time == 0.23
 
     def test_insert(self):
         self.run.insert_generation(1, 2, 3)
@@ -73,6 +76,9 @@ class TestRunEntryMatcher(TestCase):
         assert self.matcher.run.function == 3
         assert self.matcher.run.dimension == 20
         assert self.matcher.run.instance == 90
+        assert self.matcher.run.evaluations_count == 5008
+        assert self.matcher.run.dopt == 5.7707e+01
+        assert self.matcher.run.time == 105.65
 
 
 class TestLogParser(TestCase):
@@ -82,12 +88,12 @@ class TestLogParser(TestCase):
 =[DTS]=   12 /   28( 1:0,0) | 2.4e+01 | 6.4e-07 | 0.00 | 0.00 | 0.00 [ ***** ] | .  0/ 29 | 1.04e+01 | 0.00 | 0.00 | 0.05 | 0.00 |
 =[DTS]=   13 /   30( 1:0,0) | 2.4e+01 | 6.4e-07 | 0.00 | 0.00 | 0.00 [ ***** ] | .  0/ 29 | 1.04e+01 | 0.00 | 0.00 | 0.05 | 0.00 |
 Covariance function: {@covPoly, 'eye', 1}
-Covariance function: {@covPoly, 'eye', 1}
+Covariance function: @covRQiso
 =[DTS]=   14 /   31( 1:0,0) | 1.7e+01 | 4.9e-06 | 0.00 | 0.00 | 0.00 [ ***** ] | .  0/ 30 | 8.16e+00 | 0.00 | 0.00 | 0.05 | 0.00 |
-Covariance function: {@covPoly, 'eye', 1}
+Covariance function: {@covMaterniso, 5}
 Covariance function: {@covPoly, 'eye', 1}
 =[DTS]=   15 /   32( 1:0,0) | 0.0e+00 | 1.3e-05 | 0.00 | 0.00 | 0.00 [ ***** ] | .  0/ 31 | 9.34e+00 | 0.00 | 0.00 | 0.05 | 0.00 |
-  f5 in 10-D, instance 90: FEs=33 with 0 restarts, fbest-ftarget=-1.0000e-08, elapsed time [h]: 0.36
+  f5 in 10-D, instance 90: FEs=33 with 0 restarts, fbest-ftarget=1.0000e-08, elapsed time [h]: 0.36
 """
 
         self.tmpfile = os.path.join(tempfile.gettempdir(), "tmp.o123")
@@ -104,9 +110,17 @@ Covariance function: {@covPoly, 'eye', 1}
         assert struct['function'] == 5
         assert struct['dimension'] == 10
         assert struct['instance'] == 90
+        assert struct['evaluationsCount'] == 33
+        assert struct['generationsCount'] == 4
+        assert struct['distanceOptimum'] == 1e-8
+        assert struct['time'] == 0.36
+        assert struct['distinctFirstCovariances'] == 2
+        assert struct['distinctSecondCovariances'] == 2
+        assert struct["firstCovariancesHistogram"] == [0, 0, 1, 1, 0]
+        assert struct["secondCovariancesHistogram"] == [1, 0, 0, 1, 0]
 
-        assert struct['firstCovariances'] == [-1, -1, 3, 3]
-        assert struct['secondCovariances'] == [-1, -1, 3, 3]
+        assert struct['firstCovariances'] == [-1, -1, 3, 2]
+        assert struct['secondCovariances'] == [-1, -1, 0, 3]
         assert struct['evaluations'] == [28, 30, 31, 32]
 
     def tearDown(self):
