0b50e6480e5d35a790497ee320d24a27595dda71
Zbynek Pitra <jdgregorian@seznam.cz>
2020-09-03 17:43:36 +0200

    Number of processed stdout files in store_meta_output increased

--- exp/pproc/store_meta_output.sh
+++ exp/pproc/store_meta_output.sh
@@ -12,19 +12,33 @@ cd $CWD/../..
 EXPPATH="exp/experiments"
 EXPID=$1
 
-files=`ls ${EXPID}__*`
+# create list of files
+FILE_LIST=/tmp/stdout_file_list
+# find is used instead of ls due to possible large number of stdout files
+find . -maxdepth 1 -type f -iname "${EXPID}__*" > $FILE_LIST
+
 tarfile="$EXPPATH/$EXPID/${EXPID}__stdout.tgz"
-if [ ! -z "$files" ]; then
+if [ -s "${FILE_LIST}" ]; then
   if [ -f "$tarfile" ]; then
     unzipped="${tarfile/.tgz/.tar}"
     gunzip "$tarfile" # > "$unzipped"
-    tar rf "$unzipped" ${EXPID}__* && rm ${EXPID}__*
+    tar rf "$unzipped" -T ${FILE_LIST}
+    # remove stdout files
+    # xargs with "rm" is used due to possible large number of stdout files
+    # -I{} rm {} is due to absence of newlines in FILE_LIST
+    xargs -a ${FILE_LIST} -I{} rm {}
     gzip -c "$unzipped" > "$tarfile" && rm "$unzipped"
   else
-    tar czf "$tarfile" ${EXPID}__* && rm ${EXPID}__*
+    tar czf "$tarfile" -T ${FILE_LIST}
+    # remove stdout files
+    # xargs with "rm" is used due to possible large number of stdout files
+    # -I{} rm {} is due to absence of newlines in FILE_LIST
+    xargs -a ${FILE_LIST} -I{} rm {}
   fi
 else
   echo "No files to process."
 fi
 
+# remove list of stdout files
+rm $FILE_LIST
 cd "$dir"
