fe5f1f999c206288f37cf69817c47d71aad5be25
Zbynek Pitra <jdgregorian@seznam.cz>
2020-12-18 10:36:52 +0100

    EC indicator added to screen statistics

--- exp/log/DTScreenStatistics.m
+++ exp/log/DTScreenStatistics.m
@@ -11,22 +11,52 @@ classdef DTScreenStatistics < Observer
     end
 
     function notify(obj, ec, varargin)
-      % get the interesting data and process them
+    % get the interesting data from the generation, process and show them
+
+      % get EC abbreviation
+      switch class(ec)
+        case 'DoubleTrainedEC'
+          ecShort = 'DTS';
+        case 'GenerationEC'
+          ecShort = ' S ';
+        case 'LinQuadEC'
+          ecShort = 'lq';
+        case 'LmmEC'
+          ecShort = 'lmm';
+        case 'ModelAssistedEC'
+          ecShort = 'MA';
+        otherwise
+          ecShort = class(ec);
+          if strcmp(ecShort(end-1:end), 'EC')
+            ecShort = ecShort(1:end-2);
+          end
+      end
+      % each 10 generations show the header
       if (mod(ec.cmaesState.countiter, 10) == 1)
       %           #####  iter /evals(or:p,b) | Dopt |rmseR | rnkR | rnk2 |rnkVal * | Mo nD nDiR |sigm^2| aErr |smooEr| orRat| aGain|
-        fprintf('####### iter /evals(or:p,b) | D_fopt. | rmseRee | rnkR | rnk2 | .rankErrValid. | M  nData | sigma^2. | aErr |smooEr| orRat| aGain|\n');
+        fprintf('##%s## iter /evals(or:p,b) | D_fopt. | rmseRee | rnkR | rnk2 | .rankErrValid. | M  nData | sigma^2. | aErr |smooEr| orRat| aGain|\n', ...
+                '#'*ones(1, numel(ecShort)));
       end
       model = '.';
       nTrainData = 0;
+      % model successfully trained in current generation
       if (~isempty(ec.model) && ec.model.isTrained() ...
           && ec.model.trainGeneration == ec.cmaesState.countiter)
-        model = '+'; nTrainData = ec.model.getTrainsetSize(); end
+        model = '+';
+        nTrainData = ec.model.getTrainsetSize();
+      end
+      % model successfully retrained in current generation
       if (~isempty(ec.retrainedModel) && ec.retrainedModel.isTrained() ...
           && ec.retrainedModel.trainGeneration == ec.cmaesState.countiter)
-        model = '#'; nTrainData = ec.retrainedModel.getTrainsetSize(); end
+        model = '#';
+        nTrainData = ec.retrainedModel.getTrainsetSize();
+      end
       outputValues1 = [...
-          ec.cmaesState.countiter, ec.counteval, sum(ec.pop.origEvaled), ...
-          ec.nPresampledPoints, ec.usedBestPoints, ...
+          ec.cmaesState.countiter, ... % generation number
+          ec.counteval, ... % number of original-evaluated points so far
+          sum(ec.pop.origEvaled), ... % number of original-evaluated points in current generation
+          ec.nPresampledPoints, ... % number of presampled points
+          ec.usedBestPoints, ...
           ec.stats.fmin - ec.surrogateOpts.fopt, ...
           ec.stats.rmseReeval, ...
           ec.stats.rankErrReeval, ...
@@ -42,8 +72,8 @@ classdef DTScreenStatistics < Observer
       outputValues1(isnan(outputValues1)) = 0.0;
       outputValues2(isnan(outputValues2)) = 0.0;
       %         #####  iter /evals(or,p) | Dopt |rmseR | rnkR | rnk2 |rnkVal * | Mo nD nDiR |sigm^2| aErr |smooEr| orRat| aGain|
-      fprintf('=[DTS]= %4d /%5d(%2d:%1d,%1d) | %.1e | %.1e | %.2f | %.2f | %.2f %s | %s %2d/%3d | %.2e | %.2f | %.2f | %.2f | %.2f |\n', ...
-          outputValues1(:), decorateKendall(1-2*ec.stats.rankErrValid), model, outputValues2(:) ...
+      fprintf('=[%s]= %4d /%5d(%2d:%1d,%1d) | %.1e | %.1e | %.2f | %.2f | %.2f %s | %s %2d/%3d | %.2e | %.2f | %.2f | %.2f | %.2f |\n', ...
+          ecShort, outputValues1(:), decorateKendall(1-2*ec.stats.rankErrValid), model, outputValues2(:) ...
           );
     end
   end
